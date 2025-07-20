5e86c16c3672a5736bc13ca0847ae15bac35f419
Your GitHub Peter0-brandel <peter01rebrandel@gmail.com>
2025-07-20 01:46:17 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,7 +121,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="bg-white text-gray-900 font-inter antialiased relative">
+      <body className="bg-white text-gray-900 font-inter antialiased relative pb:20 lg:pb-0">
         {children}
         <BottomNav />
       </body>
